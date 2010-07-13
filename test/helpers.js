58e7ee884b5770d31216168f66ce218c99984073
Tj Holowaychuk <tj@vision-media.ca>
2010-07-13 13:49:54 -0700

    Started implementation

---
+++ test/helpers.js
@@ -0,0 +1,43 @@
+
+/*!
+ * Keys - Test Helpers
+ * Copyright(c) 2010 TJ Holowaychuk <tj@vision-media.ca>
+ * MIT Licensed
+ */
+
+exports.test = function(exports, store) {
+    var name = store.constructor.name;
+
+    // #set()
+    exports[name + '#set()'] = function(assert, beforeExit){
+        var called = 0;
+
+        store.set('foo', 'bar', function(err){
+            ++called;
+            assert.ok(!err, 'error in callback');
+        });
+
+        beforeExit(function(){
+            assert.equal(1, called);
+        });
+    };
+    
+    // #get()
+    exports[name + '#get()'] = function(assert, beforeExit){
+        var called = 0;
+
+        store.set('name', 'tj', function(err){
+            ++called;
+            assert.ok(!err, 'error in callback');
+            store.get('name', function(err, name){
+                ++called;
+                assert.ok(!err, 'error in second callback');
+                assert.equal('tj', name);
+            });
+        });
+
+        beforeExit(function(){
+            assert.equal(2, called);
+        });
+    };
+};
---
+++ test/memory.test.js
@@ -0,0 +1,9 @@
+
+/**
+ * Module dependencies.
+ */
+
+var helpers = require('./helpers'),
+    Memory = require('keys').Memory;
+
+helpers.test(exports, new Memory);
