3f6b215bed763542b795ff07858889c13a4156ce
Tj Holowaychuk <tj@vision-media.ca>
2010-08-11 15:27:48 -0700

    Tests using done()

--- test/helpers.js
+++ test/helpers.js
@@ -14,78 +14,71 @@ var sys = require('sys'),
 
 exports.test = function(exports, store, fn) {
     var name = store.constructor.name,
-        fn = fn || function(){},
-        pending = 0;
+        fn = fn || function(){};
 
     exports.setup = function(fn){
         store.clear(fn);
     };
 
     // #set()
-    ++pending;
-    exports[name + '#set()'] = function(assert){
+    exports[name + '#set()'] = function(assert, done){
         store.set('foo', 'bar', function(err){
             assert.ok(!err, 'error in callback');
-            --pending || fn();
+            done();
         });
     };
     
     // #set() binary
-    ++pending;
-    exports[name + '#set() binary'] = function(assert){
+    exports[name + '#set() binary'] = function(assert, done){
         store.set('bin', new Buffer('foobar'), function(err){
             assert.ok(!err, 'error in callback');
-            --pending || fn();
+            done();
         });
     };
     
     // #get() binary
-    ++pending;
-    exports[name + '#get() binary'] = function(assert){
+    exports[name + '#get() binary'] = function(assert, done){
         store.set('bin-foo', new Buffer('foobar'), function(err){
             store.get('bin-foo', function(err, val){
                 assert.ok(val instanceof Buffer, name + '#get() Buffer failed');
                 assert.equal('foobar', val.toString('ascii'));
-                --pending || fn();
+                done();
             });
         });
     };
     
     // #get()
-    ++pending;
-    exports[name + '#get()'] = function(assert){
+    exports[name + '#get()'] = function(assert, done){
         store.set('name', 'tj', function(err){
             store.get('name', function(err, val){
                 assert.ok(val instanceof Buffer, name + '#get() failed');
                 assert.equal('tj', val.toString('ascii'));
-                --pending || fn();
+                done();
             });
         });
     };
     
     // #remove()
-    ++pending;
-    exports[name + '#remove()'] = function(assert){
+    exports[name + '#remove()'] = function(assert, done){
         store.set('name', 'tj', function(err){
             store.remove('name', function(err){
                 store.get('name', function(err, name){
                     assert.ok(!name, '#remove() failed');
-                    --pending || fn();
+                    done();
                 });
             });
         });
     };
     
     // #has()
-    ++pending;
-    exports[name + ' #has()'] = function(assert){
+    exports[name + ' #has()'] = function(assert, done){
         store.remove('email', function(){
             store.has('email', function(err, exists){
                 assert.strictEqual(false, exists, '#has() was not false');
                 store.set('email', 'tj@vision-media.ca', function(err){
                     store.has('email', function(err, exists){
                         assert.strictEqual(true, exists, '#has() was not true');
-                        --pending || fn();
+                        done();
                     });
                 });
             });
@@ -93,17 +86,15 @@ exports.test = function(exports, store, fn) {
     };
     
     // #length()
-    ++pending;
-    exports[name + ' #length()'] = function(assert){
+    exports[name + ' #length()'] = function(assert, done){
         store.length(function(err, len){
             assert.equal('number', typeof len);
-            --pending || fn();
+            done();
         });
     };
     
     // #clear()
-    ++pending;
-    exports[name + ' #clear()'] = function(assert){
+    exports[name + ' #clear()'] = function(assert, done){
         store.set('foo', 'bar', function(){
             store.clear(function(err){
                 store.length(function(err, len){
@@ -123,7 +114,8 @@ exports.test = function(exports, store, fn) {
                                 assert.ok(keys.indexOf('two') >= 0);
                                 assert.ok(vals.indexOf('1') >= 0);
                                 assert.ok(vals.indexOf('2') >= 0);
-                                --pending || fn();
+                                done();
+                                fn();
                             });
                         });
                     });
